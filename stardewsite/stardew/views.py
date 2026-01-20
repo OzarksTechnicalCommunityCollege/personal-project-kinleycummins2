40bb0ffa652b5b3bc532c542aeb8938cced176cc
kinleycummins2 <kc0928941@otc.edu>
2026-01-20 13:50:54 -0600

    made some style changes and preparing to add another model

--- stardewsite/stardew/views.py
+++ stardewsite/stardew/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Crop
 # Create your views here.
-# View for the crop list - added a filter to sort by season (will use later when i add more crops)
+
+# View for the crop list - added a filter to sort by season (will use later when I add more crops)
 def crop_list(request):
     crops = Crop.objects.filter(season = "Spring")
     return render(request, "stardew/crop/list.html", {"crops": crops})
